68080ad05551bdce4dd8c75e014cc98d628b9c98
Paolo Piaggesi <ppiaggesi@citywire.co.uk>
2016-02-03 15:17:26 +0000

    first commit

---
+++ SpikeSwagger/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace SpikeSwagger.Controllers
+{
+    public class TestController : ApiController
+    {
+        /// <summary>
+        ///  This method returns a greeting
+        /// </summary>
+        /// <param name="id"> The name you want the greeting back.</param>
+        public string Get(string id)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return string.Empty;
+            }
+            return string.Format("Hello {0}", id);
+        }
+    }
+}
---
+++ SpikeSwagger/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpikeSwagger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Citywire Financial Publishers Ltd")]
+[assembly: AssemblyProduct("SpikeSwagger")]
+[assembly: AssemblyCopyright("Copyright © Citywire Financial Publishers Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("606314f0-b883-411d-8409-0b548615af40")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
